a6f61e898264b42e296efe42add3d6277ecb0388
Walter Morris <waltercmorris18@gmail.com>
2021-12-31 01:23:00 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+
+//2
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+
+//3
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+
+//4
+function overwriteBestCustomer(value = '') {
+    bestCustomer = value
+    return bestCustomer
+}
+
+
+//5 
+const leastFavoriteCustomer = "definitely bob"
+function changeLeastFavoriteCustomer() {
+    //leastFavotriteCustomer = 'uncertainly bob'
+    leastFavoriteCustomer = 'uncertianly bob'
+}
